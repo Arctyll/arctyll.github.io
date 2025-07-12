470e5d381476abbaeabe3d2190527da6e9afc4b1
ClassyCoder1 <classycoder1@gmail.com>
2025-07-12 18:42:27 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { createHtmlPlugin } from "vite-plugin-html";
-import { assets } from "./src/assets";
+import { assets } from "./client/src/assets";
 
 export default defineConfig({
   plugins: [
