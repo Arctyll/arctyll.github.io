214568cea3f0950741bddc632cb934a881a2ef7d
ClassyCoder1 <classycoder1@gmail.com>
2025-07-12 19:41:13 +0530

    update

--- client/src/assets.ts
+++ client/src/assets.ts
@@ -2,6 +2,8 @@ export const assets = {
   faviconPng: "/assets/favicon-96x96.png",
   faviconSvg: "/assets/favicon.svg",
   faviconIco: "/assets/favicon.ico",
-  appleIcon: "/assets/apple-touch-icon.png",
+  appleTouch: "/assets/apple-touch-icon.png",
   manifest: "/assets/site.webmanifest",
+  icon192: "/assets/web-app-manifest-192x192.png",
+  icon512: "/assets/web-app-manifest-512x512.png",
 };
