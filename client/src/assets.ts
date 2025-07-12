592722cf81d066de0d4d700477d6f42382b5cc4b
ClassyCoder1 <classycoder1@gmail.com>
2025-07-12 19:56:00 +0530

    update assets.ts

--- client/src/assets.ts
+++ client/src/assets.ts
@@ -1,10 +1,10 @@
-import faviconPng from "@/assets/favicon-96x96.png";
-import faviconSvg from "@/assets/favicon.svg";
-import faviconIco from "@/assets/favicon.ico";
-import appleTouch from "@/assets/apple-touch-icon.png";
-import manifest from "@/assets/site.webmanifest";
-import icon192 from "@/assets/web-app-manifest-192x192.png";
-import icon512 from "@/assets/web-app-manifest-512x512.png";
+import faviconPng from "@assets/favicon-96x96.png";
+import faviconSvg from "@assets/favicon.svg";
+import faviconIco from "@assets/favicon.ico";
+import appleTouch from "@assets/apple-touch-icon.png";
+import manifest from "@assets/site.webmanifest";
+import icon192 from "@assets/web-app-manifest-192x192.png";
+import icon512 from "@assets/web-app-manifest-512x512.png";
 
 export const assets = {
   faviconPng,
