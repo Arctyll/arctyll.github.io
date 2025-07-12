b26134a00e1f26762363ba141b6f64cc2f1f0ee9
ClassyCoder1 <classycoder1@gmail.com>
2025-07-12 19:16:17 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,43 +3,47 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { createHtmlPlugin } from "vite-plugin-html";
-import { assets } from "./client/src/assets.ts";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          ...assets
+export default defineConfig(async () => {
+  const assets = (await import("./client/src/assets.ts")).assets;
+  
+  return {
+    plugins: [
+      react(),
+      runtimeErrorOverlay(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            ...assets,
+          },
         },
+      }),
+      ...(process.env.NODE_ENV !== "production" &&
+        process.env.REPL_ID !== undefined ?
+        [
+          await import("@replit/vite-plugin-cartographer").then((m) =>
+            m.cartographer(),
+          ),
+        ] :
+        []),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(import.meta.dirname, "client", "src"),
+        "@shared": path.resolve(import.meta.dirname, "shared"),
+        "@assets": path.resolve(import.meta.dirname, "assets"),
       },
-    }),
-    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined ?
-      [
-        await import("@replit/vite-plugin-cartographer").then((m) =>
-          m.cartographer(),
-        ),
-      ] :
-      []),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "assets"),
     },
-  },
-  root: path.resolve(import.meta.dirname, "client"),
-  build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
+    root: path.resolve(import.meta.dirname, "client"),
+    build: {
+      outDir: path.resolve(import.meta.dirname, "dist/public"),
+      emptyOutDir: true,
     },
-  },
+    server: {
+      fs: {
+        strict: true,
+        deny: ["**/.*"],
+      },
+    },
+  };
 });
